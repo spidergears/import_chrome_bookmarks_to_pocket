4dbcb02e8ca11d07c815bba809aa0a456f6883f6
spidergears <findspidergears@gmail.com>
2015-03-26 15:13:08 +0530

    Added spinner in UI
    Removed URL listing in UI view

--- import.js
+++ import.js
@@ -2,15 +2,17 @@ var __request_code;
 var __access_token_string;
 
 function process_bookmarks ( bookmarks ) {
+    document.getElementById("progress-icon").innerHTML = "<img height='50px' width='50px' src='uploading.gif'></img>"
+    document.getElementById("progress-text").innerHTML = "Uploadig bookmarks to Pocket"
     console.log("Beginning import...")
     for ( var i =0; i < bookmarks.length; i++ ) {
         var bookmark = bookmarks[i];
         if ( bookmark.url ) {
             xmlhttp = make_xmlhttprequest("POST", "https://getpocket.com/v3/add", true)
             console.log( "Adding url: "+ bookmark.url );
-            chota_div = document.createElement('div')
-            chota_div.innerHTML= "Added Url:  "+ bookmark.url;
-            document.getElementById("progress").appendChild(chota_div)   
+            //chota_div = document.createElement('div')
+            //chota_div.innerHTML= "Added Url:  "+ bookmark.url;
+            //document.getElementById("progress").appendChild(chota_div)   
             xmlhttp.send("consumer_key="+ consumer_key +"&" + __access_token_string +"&url="+ encodeURI(bookmark.url) + "&tags=ChromeBookmarks")
         }
 
@@ -28,7 +30,7 @@ function get_redirect_url () {
 
 //TODO place your pocket consumer key here
 function get_pocket_consumer_key () {
-    return "your_pocket_consumer_key"
+    return "your_consumer_key_here"
 }
 
 function make_xmlhttprequest (method, url, flag) {
@@ -42,10 +44,17 @@ function get_request_code (consumer_key) {
     redirect_url = get_redirect_url();
     xmlhttp = make_xmlhttprequest ('POST', 'https://getpocket.com/v3/oauth/request', true) 
     xmlhttp.onreadystatechange = function () {
-        if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
-            request_code = xmlhttp.responseText.split('=')[1];
-            __request_code = request_code
-            lauch_chrome_webAuthFlow_and_return_access_token(request_code);
+        if ( xmlhttp.readyState === 4 ) {
+
+            if (xmlhttp.status === 200){
+                request_code = xmlhttp.responseText.split('=')[1];
+                __request_code = request_code
+                lauch_chrome_webAuthFlow_and_return_access_token(request_code);
+                }
+                else {
+                    document.getElementById("progress-icon").innerHTML = "<img height='50px' width='50px' src='failed.png'></img>"
+                    document.getElementById("progress-text").innerHTML = "Authentication failed!!"
+                }
         }
     }
     xmlhttp.send("consumer_key="+ consumer_key +"&redirect_uri="+ redirect_url)
@@ -77,9 +86,13 @@ function lauch_chrome_webAuthFlow_and_return_access_token (request_code) {
 }
 
 function import_my_chrome_bookmarks () {
+    document.getElementById("progress-icon").innerHTML = "<img height='50px' width='50px' src='authenticating.gif'></img>"
+    document.getElementById("progress-text").innerHTML = "Authenticating with Poket"
     consumer_key = get_pocket_consumer_key();
     get_request_code(consumer_key);
 
 }
 
-import_my_chrome_bookmarks()
+window.onload = function(){
+    import_my_chrome_bookmarks();
+};
